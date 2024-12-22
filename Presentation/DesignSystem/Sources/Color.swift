d1eb04101f28d74f00d83b095ccac6f001bbb48f
summercat <summerly812@gmail.com>
2024-12-22 17:17:56 +0900

    [PC-57] Feat: add color extension

---
+++ Presentation/DesignSystem/Sources/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  DesignSystem
+//
+//  Created by summercat on 12/22/24.
+//
+
+import SwiftUI
+
+public extension Color {
+    static let grayscaleBlack = DesignSystemAsset.Colors.grayscaleBlack.swiftUIColor
+    static let grayscaleDark1 = DesignSystemAsset.Colors.grayscaleDark1.swiftUIColor
+    static let grayscaleDark2 = DesignSystemAsset.Colors.grayscaleDark2.swiftUIColor
+    static let grayscaleDark3 = DesignSystemAsset.Colors.grayscaleDark3.swiftUIColor
+    static let grayscaleLight1 = DesignSystemAsset.Colors.grayscaleLight1.swiftUIColor
+    static let grayscaleLight2 = DesignSystemAsset.Colors.grayscaleLight2.swiftUIColor
+    static let grayscaleLight3 = DesignSystemAsset.Colors.grayscaleLight3.swiftUIColor
+    static let grayscaleWhite = DesignSystemAsset.Colors.grayscaleWhite.swiftUIColor
+    static let primaryDefault = DesignSystemAsset.Colors.primaryDefault.swiftUIColor
+    static let primaryLight = DesignSystemAsset.Colors.primaryLight.swiftUIColor
+    static let primaryMiddle = DesignSystemAsset.Colors.primaryMiddle.swiftUIColor
+    static let subDefault = DesignSystemAsset.Colors.subDefault.swiftUIColor
+    static let subLight = DesignSystemAsset.Colors.subLight.swiftUIColor
+    static let subMiddle = DesignSystemAsset.Colors.subMiddle.swiftUIColor
+}
