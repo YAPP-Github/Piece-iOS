ec462e23e22e3037a50fc4b90580c25d081ea17e
summercat <summerly812@gmail.com>
2024-12-22 13:10:14 +0900

    [PC-57] Build: generate design system module

---
+++ Presentation/DesignSystem/Project.swift
@@ -0,0 +1,10 @@
+//
+//  Project.swift
+//
+//  Created by summercat on 12/22/24.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.dynamicResourceFramework(name: Modules.Presentation.DesignSystem.rawValue)
