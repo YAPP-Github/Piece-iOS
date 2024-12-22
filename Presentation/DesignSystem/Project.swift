5339cf0407b443cc12f2777ac41ce1077b80a85f
summercat <summerly812@gmail.com>
2024-12-22 16:03:41 +0900

    [PC-57] Feat: add font assets

--- Presentation/DesignSystem/Project.swift
+++ Presentation/DesignSystem/Project.swift
@@ -7,4 +7,23 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 
-let project = Project.dynamicResourceFramework(name: Modules.Presentation.DesignSystem.rawValue)
+private let fonts = [
+  "Pretendard-Light.otf",
+  "Pretendard-ExtraLight.otf",
+  "Pretendard-Thin.otf",
+  "Pretendard-Bold.otf",
+  "Pretendard-SemiBold.otf",
+  "Pretendard-Medium.otf",
+  "Pretendard-Black.otf",
+  "Pretendard-Regular.otf",
+  "Pretendard-ExtraBold.otf",
+]
+
+private let infoPlist: [String: Plist.Value] = [
+  "Fonts provided by application": .array(fonts.map { .string($0) })
+]
+
+let project = Project.dynamicResourceFramework(
+  name: Modules.Presentation.DesignSystem.rawValue,
+  infoPlist: .extendingDefault(with: infoPlist)
+)
