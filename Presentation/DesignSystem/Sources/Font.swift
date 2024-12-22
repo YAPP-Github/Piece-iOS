976edbfaf3f6ee90f4f7076bfc505c698186a7ed
summercat <summerly812@gmail.com>
2024-12-22 17:18:38 +0900

    [PC-57] Feat: create font view modifier to apply lineheight

---
+++ Presentation/DesignSystem/Sources/Font.swift
@@ -0,0 +1,75 @@
+//
+//  Font.swift
+//  DesignSystem
+//
+//  Created by summercat on 12/22/24.
+//
+
+import SwiftUI
+import UIKit
+
+public enum Fonts {
+  public enum Pretendard {
+    case heading_XL_SB
+    case heading_L_SB
+    case heading_M_SB
+    case heading_S_SB
+    case heading_S_M
+    case body_M_SB
+    case body_M_M
+    case body_M_R
+    case body_S_SB
+    case body_S_M
+    case caption_M_M
+    
+    var swiftUIFont: Font {
+      switch self {
+      case .heading_XL_SB: DesignSystemFontFamily.Pretendard.semiBold.swiftUIFont(size: 28)
+      case .heading_L_SB: DesignSystemFontFamily.Pretendard.semiBold.swiftUIFont(size: 24)
+      case .heading_M_SB: DesignSystemFontFamily.Pretendard.semiBold.swiftUIFont(size: 20)
+      case .heading_S_SB: DesignSystemFontFamily.Pretendard.semiBold.swiftUIFont(size: 18)
+      case .heading_S_M: DesignSystemFontFamily.Pretendard.medium.swiftUIFont(size: 18)
+      case .body_M_SB: DesignSystemFontFamily.Pretendard.semiBold.swiftUIFont(size: 16)
+      case .body_M_M: DesignSystemFontFamily.Pretendard.medium.swiftUIFont(size: 16)
+      case .body_M_R: DesignSystemFontFamily.Pretendard.regular.swiftUIFont(size: 16)
+      case .body_S_SB: DesignSystemFontFamily.Pretendard.semiBold.swiftUIFont(size: 14)
+      case .body_S_M: DesignSystemFontFamily.Pretendard.medium.swiftUIFont(size: 14)
+      case .caption_M_M: DesignSystemFontFamily.Pretendard.medium.swiftUIFont(size: 12)
+      }
+    }
+    
+    var uiFont: UIFont {
+      switch self {
+      case .heading_XL_SB: DesignSystemFontFamily.Pretendard.semiBold.font(size: 28)
+      case .heading_L_SB: DesignSystemFontFamily.Pretendard.semiBold.font(size: 24)
+      case .heading_M_SB: DesignSystemFontFamily.Pretendard.semiBold.font(size: 20)
+      case .heading_S_SB: DesignSystemFontFamily.Pretendard.semiBold.font(size: 18)
+      case .heading_S_M: DesignSystemFontFamily.Pretendard.medium.font(size: 18)
+      case .body_M_SB: DesignSystemFontFamily.Pretendard.semiBold.font(size: 16)
+      case .body_M_M: DesignSystemFontFamily.Pretendard.medium.font(size: 16)
+      case .body_M_R: DesignSystemFontFamily.Pretendard.regular.font(size: 16)
+      case .body_S_SB: DesignSystemFontFamily.Pretendard.semiBold.font(size: 14)
+      case .body_S_M: DesignSystemFontFamily.Pretendard.medium.font(size: 14)
+      case .caption_M_M: DesignSystemFontFamily.Pretendard.medium.font(size: 12)
+      }
+    }
+    
+    var lineHeight: CGFloat {
+      switch self {
+      case .heading_XL_SB: 40
+      case .heading_L_SB: 32
+      case .heading_M_SB: 24
+      case .heading_S_SB, .heading_S_M: 22
+      case .body_M_SB, .body_M_M, .body_M_R: 24
+      case .body_S_SB, .body_S_M: 18
+      case .caption_M_M: 16
+      }
+    }
+    
+    var letterSpacing: CGFloat {
+      switch self {
+      default: -0.01 // All cases use letter spacing of -1%
+      }
+    }
+  }
+}
