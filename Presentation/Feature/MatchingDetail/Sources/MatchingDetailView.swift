3904fc9253780722c9757b659ae13722e825a3a0
summercat <summerly812@gmail.com>
2025-01-02 23:15:24 +0900

    [PC-77] Feat: 매칭 디테일 모듈 생성

---
+++ Presentation/Feature/MatchingDetail/Sources/MatchingDetailView.swift
@@ -0,0 +1,15 @@
+//
+// MatchingDetailView.swift
+// MatchingDetail
+//
+// Created by summercat on 2025/01/02.
+//
+
+import SwiftUI
+import DesignSystem
+
+struct MatchingDetailView: View {
+  var body: some View {
+  
+  }
+}
