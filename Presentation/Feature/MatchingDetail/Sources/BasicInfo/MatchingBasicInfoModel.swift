31d65404c39e6ae8ba44178775cad560a2b4cfb3
summercat <summerly812@gmail.com>
2025-01-05 00:23:01 +0900

    [PC-193] Feat: 매칭 상세 기본 정보 화면 구현
    - navigation bar는 추후 추가 필요

---
+++ Presentation/Feature/MatchingDetail/Sources/BasicInfo/MatchingBasicInfoModel.swift
@@ -0,0 +1,20 @@
+//
+//  MatchingBasicInfoModel.swift
+//  MatchingDetail
+//
+//  Created by summercat on 1/4/25.
+//
+
+import Observation
+
+struct MatchingBasicInfoModel {
+  let description: String
+  let nickname: String
+  let age: Int
+  let birthYear: Int
+  let height: Int
+  let religion: String
+  let region: String
+  let job: String
+  let isSmoker: Bool
+}
