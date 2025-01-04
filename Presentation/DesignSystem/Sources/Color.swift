cbabc3d63d8569c9ae900753ace825cd9d9ed433
summercat <summerly812@gmail.com>
2025-01-04 10:49:58 +0900

    [PC-193] Feat: alpha, systemError 컬러 애셋 추가

--- Presentation/DesignSystem/Sources/Color.swift
+++ Presentation/DesignSystem/Sources/Color.swift
@@ -8,18 +8,22 @@
 import SwiftUI
 
 public extension Color {
-    static let grayscaleBlack = DesignSystemAsset.Colors.grayscaleBlack.swiftUIColor
-    static let grayscaleDark1 = DesignSystemAsset.Colors.grayscaleDark1.swiftUIColor
-    static let grayscaleDark2 = DesignSystemAsset.Colors.grayscaleDark2.swiftUIColor
-    static let grayscaleDark3 = DesignSystemAsset.Colors.grayscaleDark3.swiftUIColor
-    static let grayscaleLight1 = DesignSystemAsset.Colors.grayscaleLight1.swiftUIColor
-    static let grayscaleLight2 = DesignSystemAsset.Colors.grayscaleLight2.swiftUIColor
-    static let grayscaleLight3 = DesignSystemAsset.Colors.grayscaleLight3.swiftUIColor
-    static let grayscaleWhite = DesignSystemAsset.Colors.grayscaleWhite.swiftUIColor
-    static let primaryDefault = DesignSystemAsset.Colors.primaryDefault.swiftUIColor
-    static let primaryLight = DesignSystemAsset.Colors.primaryLight.swiftUIColor
-    static let primaryMiddle = DesignSystemAsset.Colors.primaryMiddle.swiftUIColor
-    static let subDefault = DesignSystemAsset.Colors.subDefault.swiftUIColor
-    static let subLight = DesignSystemAsset.Colors.subLight.swiftUIColor
-    static let subMiddle = DesignSystemAsset.Colors.subMiddle.swiftUIColor
+  static let grayscaleBlack = DesignSystemAsset.Colors.grayscaleBlack.swiftUIColor
+  static let grayscaleDark1 = DesignSystemAsset.Colors.grayscaleDark1.swiftUIColor
+  static let grayscaleDark2 = DesignSystemAsset.Colors.grayscaleDark2.swiftUIColor
+  static let grayscaleDark3 = DesignSystemAsset.Colors.grayscaleDark3.swiftUIColor
+  static let grayscaleLight1 = DesignSystemAsset.Colors.grayscaleLight1.swiftUIColor
+  static let grayscaleLight2 = DesignSystemAsset.Colors.grayscaleLight2.swiftUIColor
+  static let grayscaleLight3 = DesignSystemAsset.Colors.grayscaleLight3.swiftUIColor
+  static let grayscaleWhite = DesignSystemAsset.Colors.grayscaleWhite.swiftUIColor
+  static let primaryDefault = DesignSystemAsset.Colors.primaryDefault.swiftUIColor
+  static let primaryLight = DesignSystemAsset.Colors.primaryLight.swiftUIColor
+  static let primaryMiddle = DesignSystemAsset.Colors.primaryMiddle.swiftUIColor
+  static let subDefault = DesignSystemAsset.Colors.subDefault.swiftUIColor
+  static let subLight = DesignSystemAsset.Colors.subLight.swiftUIColor
+  static let subMiddle = DesignSystemAsset.Colors.subMiddle.swiftUIColor
+  static let alphaWhite10 = DesignSystemAsset.Colors.alphaWhite10.swiftUIColor
+  static let alphaWhite60 = DesignSystemAsset.Colors.alphaWhite60.swiftUIColor
+  static let alphaBlack40 = DesignSystemAsset.Colors.alphaBlack40.swiftUIColor
+  static let systemError = DesignSystemAsset.Colors.systemError.swiftUIColor
 }
