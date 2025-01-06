856c46b998ba1ec37c9a19075da611fcc9356ecb
SeoU <tjdnfkd1225@naver.com>
2025-01-06 17:42:08 +0900

    [PC-192] Feat: DateFormatter+Extension 생성

---
+++ Presentation/Feature/MatchingMain/Sources/DateFormatter+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  DateFormatter+Extension.swift
+//  MatchingMain
+//
+//  Created by eunseou on 1/6/25.
+//
+
+import Foundation
+
+extension DateFormatter {
+  static func formattedTimeString(from timeInterval: TimeInterval) -> String {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "HH:mm:ss"
+    formatter.locale = Locale.current
+    formatter.timeZone = TimeZone.current
+    let date = Date(timeIntervalSince1970: timeInterval)
+    return formatter.string(from: date)
+  }
+}
