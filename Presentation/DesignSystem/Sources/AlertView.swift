2bb7ca5d3c07154dc2422f8629e677ca7105d761
SeoU <tjdnfkd1225@naver.com>
2025-01-06 18:00:23 +0900

    [PC-201] Fix: AlertType 제거

--- Presentation/DesignSystem/Sources/AlertView.swift
+++ Presentation/DesignSystem/Sources/AlertView.swift
@@ -8,13 +8,7 @@
 import SwiftUI
 
 public struct AlertView: View {
-  public enum AlertType {
-    case defaultType
-    case iconType
-  }
-  
   public init(
-    type: AlertType,
     icon: Image? = nil,
     title: String,
     message: String,
@@ -23,7 +17,6 @@ public struct AlertView: View {
     firstButtonAction: @escaping () -> Void,
     secondButtonAction: @escaping () -> Void
   ) {
-    self.type = type
     self.icon = icon
     self.title = title
     self.message = message
@@ -36,7 +29,6 @@ public struct AlertView: View {
   public var body: some View {
     VStack {
       AlertTopView(
-        type: type,
         icon: icon,
         title: title,
         message: message
@@ -56,7 +48,6 @@ public struct AlertView: View {
     )
   }
   
-  private let type: AlertType
   private let icon: Image?
   private let title: String
   private let message: String
@@ -69,7 +60,7 @@ public struct AlertView: View {
 private struct AlertTopView: View {
   var body: some View {
     VStack(spacing: 8) {
-      if type == .iconType, let icon {
+      if let icon {
         icon
       }
       Text(title)
@@ -85,7 +76,6 @@ private struct AlertTopView: View {
     .padding(.horizontal, 20)
   }
   
-  let type: AlertView.AlertType
   let icon: Image?
   let title: String
   let message: String
@@ -125,7 +115,6 @@ private struct AlertBottomView: View {
     Color.grayscaleBlack.ignoresSafeArea()
     VStack{
       AlertView(
-        type: .defaultType,
         title: "수줍은 수달님과의 인연을 이어가시겠습니까?",
         message: "서로 매칭을 수락하면, 연락처가 공개됩니다.",
         firstButtonText: "뒤로",
@@ -134,7 +123,6 @@ private struct AlertBottomView: View {
         secondButtonAction: {}
       )
       AlertView(
-        type: .iconType,
         icon: DesignSystemAsset.Icons.matchingModeCheck20.swiftUIImage,
         title: "수줍은 수달님과의 인연을 이어가시겠습니까?",
         message: "서로 매칭을 수락하면, 연락처가 공개됩니다.",
