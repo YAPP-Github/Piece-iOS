056d8c89c5297591cb9ae0de98c5dbf9ed3994bf
SeoU <tjdnfkd1225@naver.com>
2025-01-08 18:49:59 +0900

    [PC-192] Fix: MatchingMainView 구조 변경에 따른 ViewModel 변경
    - computed property를 사용하여 View를 구성함으로 서 과도한 파일 분리 감축
    - 동적으로 변동 가능성있는 값들을 고려하여 Viewmodel에 추가

--- Presentation/Feature/MatchingMain/Sources/MatchingMainViewModel.swift
+++ Presentation/Feature/MatchingMain/Sources/MatchingMainViewModel.swift
@@ -10,61 +10,43 @@ import Observation
 
 @Observable
 final class MatchingMainViewModel {
-    private enum Constant {
-        static let defaultDescription = "[나를 표현하는 한마디]"
-        static let defaultName = "[닉네임]"
-        static let defaultAge = "02"
-        static let defaultLocation = "대구광역시"
-        static let defaultJob = "학생"
-        static let defaultTags = [
-            "바깥 데이트 스킨십도 가능",
-            "함께 술을 즐기고 싶어요",
-            "커밍아웃은 가까운 친구에게만 했어요",
-            "연락은 바쁘더라도 자주",
-            "최대 너비 260. 두 줄 노출 가능. 최대 너비 260. 두 줄 노출 가능."
-        ]
+  enum Action {
+    case tapProfileInfo
+    case acceptMatching
+  }
+  
+  private(set) var description: String
+  private(set) var name: String
+  private(set) var age: String
+  private(set) var location: String
+  private(set) var job: String
+  private(set) var tags: [String]
+  var buttonTitle: String
+  var matchingStatus: MatchingAnswer.MatchingStatus
+  
+  init(
+    description: String,
+    name: String,
+    age: String,
+    location: String,
+    job: String,
+    tags: [String],
+    buttonTitle: String,
+    matchingStatus: MatchingAnswer.MatchingStatus
+  ) {
+    self.description = description
+    self.name = name
+    self.age = age
+    self.location = location
+    self.job = job
+    self.tags = tags
+    self.buttonTitle = buttonTitle
+    self.matchingStatus = matchingStatus
+  }
+  
+  func handleAction(_ action: Action) {
+    switch action {
+    default: return
     }
-    
-    enum Action {
-        case fetchProfile
-    }
-    
-    private(set) var description: String
-    private(set) var name: String
-    private(set) var age: String
-    private(set) var location: String
-    private(set) var job: String
-    private(set) var tags: [String]
-    
-    init(
-        description: String = Constant.defaultDescription,
-        name: String = Constant.defaultName,
-        age: String = Constant.defaultAge,
-        location: String = Constant.defaultLocation,
-        job: String = Constant.defaultJob,
-        tags: [String] = Constant.defaultTags
-    ) {
-        self.description = description
-        self.name = name
-        self.age = age
-        self.location = location
-        self.job = job
-        self.tags = tags
-    }
-    
-    func handleAction(_ action: Action) {
-        switch action {
-        case .fetchProfile:
-            fetchProfile()
-        }
-    }
-    
-    private func fetchProfile() {
-        description = Constant.defaultDescription
-        name = Constant.defaultName
-        age = Constant.defaultAge
-        location = Constant.defaultLocation
-        job = Constant.defaultJob
-        tags = Constant.defaultTags
-    }
+  }
 }
