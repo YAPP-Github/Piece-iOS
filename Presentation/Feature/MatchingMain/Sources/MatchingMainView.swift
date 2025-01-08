9a4cd7f55cca8a0bdda5c843c5bb5fa32ec69ebd
SeoU <tjdnfkd1225@naver.com>
2025-01-08 18:30:33 +0900

    [PC-192] Fix: 타이머뷰 파일 분리 및 네이밍 변경

--- Presentation/Feature/MatchingMain/Sources/MatchingMainView.swift
+++ Presentation/Feature/MatchingMain/Sources/MatchingMainView.swift
@@ -16,33 +16,14 @@ struct MatchingMainView: View {
     ZStack {
       Color.grayscaleBlack.edgesIgnoringSafeArea(.all)
       VStack {
-        MatchingTimerView(matchingTimerViewModel: matchingTimerViewModel)
         MatchingProfileCardView(matchingMainViewModel: matchingMainViewModel)
+        MatchingTimer(matchingTimerViewModel: matchingTimerViewModel)
       }
       .padding(.horizontal, 20)
     }
   }
-}
-
-// MARK: - 타이머
-private struct MatchingTimerView: View {
-  @Bindable var matchingTimerViewModel: MatchingTimerViewModel
   
-  var body: some View {
-    HStack(spacing: 4) {
-      Text("소중한 인연이 사라지기까지")
-      Text(matchingTimerViewModel.state.timeString)
-        .pretendard(.body_S_SB)
-        .foregroundColor(.subDefault)
-      Text("남았어요")
     }
-    .pretendard(.body_S_M)
-    .foregroundColor(.grayscaleLight1)
-    .padding(.vertical, 12)
-    .padding(.horizontal, 33)
-    .fixedSize(horizontal: true, vertical: false)
-    .background(Color.grayscaleWhite.opacity(0.1))
-    .cornerRadius(8)
   }
 }
 
