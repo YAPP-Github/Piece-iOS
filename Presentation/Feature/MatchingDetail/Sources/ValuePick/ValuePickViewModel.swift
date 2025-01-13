14c9f2bc2261778b4ecaa383081097b535771d24
summercat <summerly812@gmail.com>
2025-01-14 01:36:27 +0900

    [PC-196] Chore: ValuePickTab 별도 파일로 분리

--- Presentation/Feature/MatchingDetail/Sources/ValuePick/ValuePickViewModel.swift
+++ Presentation/Feature/MatchingDetail/Sources/ValuePick/ValuePickViewModel.swift
@@ -7,24 +7,6 @@
 
 import Foundation
 
-enum ValuePickTab: CaseIterable, Identifiable {
-  case all
-  case same
-  case different
-  
-  var id: String {
-    return self.description
-  }
-  
-  var description: String {
-    switch self {
-    case .all: "전체"
-    case .same: "나와 같은"
-    case .different: "나와 다른"
-    }
-  }
-}
-
 @Observable
 final class ValuePickViewModel {
   private enum Constant {
