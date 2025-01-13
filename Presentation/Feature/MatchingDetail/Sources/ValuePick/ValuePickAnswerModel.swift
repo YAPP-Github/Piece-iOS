1bc55da16fe0febd3f6b3311fa0842f1f6a4b78d
summercat <summerly812@gmail.com>
2025-01-13 23:57:25 +0900

    [PC-196] Feat: 가치관 Pick 카드 UI 구현

---
+++ Presentation/Feature/MatchingDetail/Sources/ValuePick/ValuePickAnswerModel.swift
@@ -0,0 +1,12 @@
+//
+//  ValuePickAnswerModel.swift
+//  MatchingDetail
+//
+//  Created by summercat on 1/13/25.
+//
+
+struct ValuePickAnswerModel: Identifiable {
+  let id: Int
+  let content: String
+  let isSelected: Bool
+}
