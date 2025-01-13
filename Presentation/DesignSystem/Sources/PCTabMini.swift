ec666b41544aec31e0cc2456fef27a69fa96b149
summercat <summerly812@gmail.com>
2025-01-14 01:19:20 +0900

    [PC-196] Feat: PCTabMini 컴포넌트 구현

---
+++ Presentation/DesignSystem/Sources/PCTabMini.swift
@@ -0,0 +1,43 @@
+//
+//  PCTabMini.swift
+//  DesignSystem
+//
+//  Created by summercat on 1/14/25.
+//
+
+import SwiftUI
+
+public struct PCMiniTab<Content: View>: View {
+  public init(
+    isSelected: Bool,
+    @ViewBuilder content: () -> Content
+  ) {
+    self.isSelected = isSelected
+    self.content = content()
+  }
+  
+  public var body: some View {
+    content
+      .pretendard(.body_M_M)
+      .foregroundStyle(isSelected ? Color.grayscaleBlack : Color.grayscaleDark3)
+      .frame(maxWidth: .infinity)
+      .padding(.horizontal, 16)
+      .padding(.vertical, 12)
+      .contentShape(Rectangle())
+  }
+  
+  @State var isSelected: Bool
+  @ViewBuilder let content: Content
+}
+
+#Preview {
+  HStack(alignment: .top) {
+    PCMiniTab(isSelected: true) {
+      Text("전체")
+    }
+    
+    PCMiniTab(isSelected: false) {
+      Text("전체")
+    }
+  }
+}
