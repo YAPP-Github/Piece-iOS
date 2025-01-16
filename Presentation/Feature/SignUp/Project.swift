184b8365ca4e242520f0dd8677e616206d50c9e5
SeoU <tjdnfkd1225@naver.com>
2025-01-16 15:58:56 +0900

    [PC-391] Feat: signup 모듈 생성

---
+++ Presentation/Feature/SignUp/Project.swift
@@ -0,0 +1,18 @@
+//
+//  Project.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by eunseou on 1/12/25.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.staticLibrary(
+  name: Modules.Presentation.SignUp.rawValue,
+  dependencies: [
+    .presentation(target: .DesignSystem),
+    .utility(target: .PCFoundationExtension)
+  ]
+)
+
