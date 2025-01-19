e491d64649c49f7bff397bc040f8d552e6757ce9
SeoU <tjdnfkd1225@naver.com>
2025-01-20 02:43:28 +0900

    [PC-391] Feat: 약관동의뷰 UI 구현

---
+++ Presentation/Feature/SignUp/Sources/TermsAgreement/TermsAgreementViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  TermsOfServiceViewModel.swift
+//  SignUp
+//
+//  Created by eunseou on 1/15/25.
+//
+
+import SwiftUI
+import Observation
+import DesignSystem
+
+@Observable
+final class TermsAgreementViewModel {
+  enum Action {
+    case toggleAll
+    case toggleTerm(id: Int)
+    case tapTermURL(url: String)
+    case tapNextButton
+    case tapBackButton
+  }
+  
+  init(terms: [TermModel]) {
+    self.terms = terms
+  }
+  
+  var terms: [TermModel]
+  var isAllChecked: Bool {
+    terms.allSatisfy { $0.isChecked }
+  }
+  var nextButtonType: RoundedButton.ButtonType {
+    isAllChecked ? .solid : .disabled
+  }
+  
+  func handleAction(_ action: Action) {
+    switch action {
+    case .toggleAll:
+      let newState = !isAllChecked
+      terms.indices.forEach {
+        terms[$0].isChecked = newState
+      }
+    case .toggleTerm(let id):
+      if let index = terms.firstIndex(where: { $0.id == id }) {
+        terms[index].isChecked.toggle()
+      }
+    default:
+      return
+    }
+  }
+}
