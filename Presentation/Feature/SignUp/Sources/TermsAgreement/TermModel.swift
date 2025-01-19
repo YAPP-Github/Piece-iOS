b27371406a07fe41a901cd7fd6fbcbcdd3249514
SeoU <tjdnfkd1225@naver.com>
2025-01-20 02:42:43 +0900

    [PC-391] Feat: TermModel 생성

---
+++ Presentation/Feature/SignUp/Sources/TermsAgreement/TermModel.swift
@@ -0,0 +1,15 @@
+//
+//  TermModel.swift
+//  SignUp
+//
+//  Created by eunseou on 1/16/25.
+//
+
+import SwiftUI
+
+struct TermModel: Identifiable {
+  let id: Int
+  let title: String
+  let url: String
+  var isChecked: Bool
+}
