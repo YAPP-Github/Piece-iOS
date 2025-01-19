f9feca1b7e1764f16938845f41b6b6b0af63b5ce
SeoU <tjdnfkd1225@naver.com>
2025-01-20 07:20:27 +0900

    [PC-393] Feat: 권한 허가 View와 Viewmodel 생성

---
+++ Presentation/Feature/SignUp/Sources/PermissionRequest/PermissionRequestViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  PermissionRequestViewModel.swift
+//  SignUp
+//
+//  Created by eunseou on 1/15/25.
+//
+
+import SwiftUI
+import DesignSystem
+import Observation
+
+@Observable
+final class PermissionRequestViewModel {
+  var isCameraPermissionGranted: Bool = false // 카메라 권한
+  var isNotificationPermissionGranted: Bool = false // 알림 권한
+  var isContactsPermissionGranted: Bool = false // 연락처 권한
+  var shouldShowSettingsAlert: Bool = false // 권한 요청 1회 거절 시 -> 설정창으로 보내기 위한 flag
+  var nextButtonType: RoundedButton.ButtonType {
+    isCameraPermissionGranted ? .solid : .disabled
+  }
+  
+  enum Action {
+    case showShettingAlert
+    case tapNextButton
+    case tapBackButton
+  }
+  
+  func handleAction(_ action: Action) {
+    switch action {
+    default: return
+    }
+  }
+}
