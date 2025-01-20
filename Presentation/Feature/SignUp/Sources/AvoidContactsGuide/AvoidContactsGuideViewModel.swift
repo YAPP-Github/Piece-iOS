d951896ebbf9c34eb019118449bac98a5a5414b0
SeoU <tjdnfkd1225@naver.com>
2025-01-20 17:53:48 +0900

    [PC-394] Feat: 지인 피하기 기능 안내 View와 ViewModel 생성

---
+++ Presentation/Feature/SignUp/Sources/AvoidContactsGuide/AvoidContactsGuideViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  AvoidContactsGuideViewModel.swift
+//  SignUp
+//
+//  Created by eunseou on 1/17/25.
+//
+
+import SwiftUI
+import Observation
+
+@Observable
+final class AvoidContactsGuideViewModel {
+  enum Action {
+    case tapBlockContactButton
+    case tapBackButton
+  }
+  
+  func handleAction(_ action: Action) {
+    switch action {
+    default :
+      return
+    }
+  }
+}
