e8b805fa2e7ff5faadd76c9d2d68fbc3486dae95
summercat <summerly812@gmail.com>
2025-01-30 02:08:22 +0900

    [PC-401] Feat: 화면 전환을 담당하는 Coordinator 객체 구현

---
+++ Presentation/Coordinator/Sources/Coordinator.swift
@@ -0,0 +1,18 @@
+//
+//  Coordinator.swift
+//  Coordinator
+//
+//  Created by summercat on 1/30/25.
+//
+
+import MatchingDetail
+import Router
+import SwiftUI
+import UseCases
+
+public struct Coordinator {
+  @ViewBuilder
+  public static func view(for route: Route) -> some View {
+      EmptyView()
+  }
+}
