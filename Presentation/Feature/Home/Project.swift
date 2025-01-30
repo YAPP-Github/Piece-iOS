93540f684e52122eee640dc22f072a3d62ff6575
summercat <summerly812@gmail.com>
2025-01-30 20:55:41 +0900

    [PC-447] Feat: 앱의 홈 화면 container 역할을 하는 Home모듈 생성

---
+++ Presentation/Feature/Home/Project.swift
@@ -0,0 +1,18 @@
+//
+// Project.swift
+// Home
+//
+// Created by summercat on 2025/01/30.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.staticLibrary(
+  name: Modules.Presentation.Home.rawValue,
+  dependencies: [
+    .presentation(target: .DesignSystem),
+    .presentation(target: .Router),
+    .presentation(target: .MatchingMain),
+  ]
+)
