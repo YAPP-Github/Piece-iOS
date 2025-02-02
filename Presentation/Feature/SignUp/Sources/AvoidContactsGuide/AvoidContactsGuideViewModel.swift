056d37a2b3e5b1ba99498c2d6d06bd36fd7949ed
SeoU <tjdnfkd1225@naver.com>
2025-02-03 00:39:27 +0900

    [PC-394] Feat: 연락처 권한 검사 로직, alert 추가

--- Presentation/Feature/SignUp/Sources/AvoidContactsGuide/AvoidContactsGuideViewModel.swift
+++ Presentation/Feature/SignUp/Sources/AvoidContactsGuide/AvoidContactsGuideViewModel.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import Observation
+import UseCases
 
 @Observable
 final class AvoidContactsGuideViewModel {
@@ -14,18 +15,39 @@ final class AvoidContactsGuideViewModel {
     case tapAccepetButton
     case tapBackButton
     case tapDenyButton
+    case showShettingAlert
+    case cancelAlert
   }
   
   var showToast = false
+  var isPresentedAlert: Bool = false
   private var dismissAction: (() -> Void)?
+  private var navigationAction: (() -> Void)?
+  private let contactsPermissionUseCase: ContactsPermissionUseCase
   
   func handleAction(_ action: Action) {
     switch action {
+    case .tapAccepetButton:
+      Task {
+        await handleAcceptButtonTap()
+      }
     case .tapBackButton:
       dismissAction?()
-    default :
-      return
+    case .tapDenyButton:
+      navigationAction?()
+    case .showShettingAlert:
+      openSettings()
+    case .cancelAlert:
+      isPresentedAlert = false
     }
+  }
+  
+  init(
+    showToast: Bool = false,
+    contactsPermissionUseCase: ContactsPermissionUseCase
+  ) {
+    self.showToast = showToast
+    self.contactsPermissionUseCase = contactsPermissionUseCase
   }
   
   func setDismissAction(_ dismiss: @escaping () -> Void) {
@@ -33,9 +55,32 @@ final class AvoidContactsGuideViewModel {
   }
   
   @MainActor
+  private func handleAcceptButtonTap() async {
+    do {
+      let isAuthorized = try await contactsPermissionUseCase.execute()
+      
+      if isAuthorized {
+        await isToastVisible()
+        navigationAction?()
+      } else {
+        isPresentedAlert = true
+      }
+    } catch {
+      print("\(error.localizedDescription)")
+    }
+  }
+  
+  @MainActor
   private func isToastVisible() async {
     showToast = true
     try? await Task.sleep(for: .seconds(3))
     showToast = false
+  }
+  
+  private func openSettings() {
+    guard let url = URL(string: UIApplication.openSettingsURLString),
+          UIApplication.shared.canOpenURL(url) else { return }
+    
+    UIApplication.shared.open(url)
   }
 }
