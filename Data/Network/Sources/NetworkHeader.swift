20d5247d091a220d6baee580425481f6245dc90e
SeoU <tjdnfkd1225@naver.com>
2025-02-04 05:03:30 +0900

    [PC-90] Feat: 네트워크 헤더값 요소 enum화

---
+++ Data/Network/Sources/NetworkHeader.swift
@@ -0,0 +1,14 @@
+//
+//  PCHeader.swift
+//  Network
+//
+//  Created by eunseou on 2/1/25.
+//
+
+import Foundation
+
+enum NetworkHeader {
+  static let contentType = "Content-Type"
+  static let applicationJson = "application/json"
+  static let multipartFormData = "multipart/form-data"
+}
