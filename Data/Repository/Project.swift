b91fa94efe21f286e9abde8dd573574f180aaa4a
SeoU <tjdnfkd1225@naver.com>
2025-02-04 18:25:56 +0900

    [PC-90] Feat: Repository 모듈 생성

---
+++ Data/Repository/Project.swift
@@ -0,0 +1,18 @@
+//
+//  Project.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by eunseou on 2/2/25.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.dynamicFramework(
+  name: Modules.Data.Repository.rawValue,
+  dependencies: [
+    .domain(target: .RepositoryInterfaces),
+    .domain(target: .Entities),
+    .data(target: .Network)
+  ]
+)
