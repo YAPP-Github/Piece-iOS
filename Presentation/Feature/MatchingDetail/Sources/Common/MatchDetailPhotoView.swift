2fd8f40e32e7cb36c1910f77de4ca3c61390d313
summercat <summerly812@gmail.com>
2025-02-06 01:00:39 +0900

    [PC-490] Feat: 매칭 상세 사진 화면에 navigation 구현

--- Presentation/Feature/MatchingDetail/Sources/Common/MatchDetailPhotoView.swift
+++ Presentation/Feature/MatchingDetail/Sources/Common/MatchDetailPhotoView.swift
@@ -6,10 +6,14 @@
 //
 
 import DesignSystem
+import Router
 import SwiftUI
 
 struct MatchDetailPhotoView: View {
   private let uri: String
+  @Environment(Router.self) private var router: Router
+  @Environment(\.dismiss) private var dismiss
+  @State private var isAlertPresented: Bool = false
   
   init(uri: String) {
     self.uri = uri
@@ -29,7 +33,7 @@ struct MatchDetailPhotoView: View {
         titleColor: .grayscaleWhite,
         rightIcon: DesignSystemAsset.Icons.close32.swiftUIImage
       ) {
-        
+        dismiss()
       }
       
       Spacer()
@@ -49,12 +53,27 @@ struct MatchDetailPhotoView: View {
         buttonText: "매칭 수락하기",
         rounding: true
       ) {
-        
+        isAlertPresented.toggle()
+      }
+    }
+    .pcAlert(isPresented: $isAlertPresented) {
+      AlertView(
+        title: "수줍은 수달님과의\n인연을 이어가시겠습니까?",
+        message: "서로 매칭을 수락하면, 연락처가 공개됩니다.",
+        firstButtonText: "뒤로",
+        secondButtonText: "매칭 수락하기"
+      ) {
+        isAlertPresented = false
+      } secondButtonAction: {
+        dismiss()
+        router.popToRoot()
       }
     }
   }
 }
 
 #Preview {
-  MatchDetailPhotoView(uri: "https://www.thesprucepets.com/thmb/AyzHgPQM_X8OKhXEd8XTVIa-UT0=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-145577979-d97e955b5d8043fd96747447451f78b7.jpg")
+  let uri = "https://www.thesprucepets.com/thmb/AyzHgPQM_X8OKhXEd8XTVIa-UT0=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-145577979-d97e955b5d8043fd96747447451f78b7.jpg"
+  MatchDetailPhotoView(uri: uri)
+    .environment(Router())
 }
