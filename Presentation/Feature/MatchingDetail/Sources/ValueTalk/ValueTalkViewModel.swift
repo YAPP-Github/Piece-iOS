2710a1bfdc181ca1faddf6dcb3da09a8a2b33cb5
summercat <summerly812@gmail.com>
2025-02-07 00:47:38 +0900

    [PC-490] Feat: 가치관 Talk 화면에서 UseCase로부터 데이터를 받아와 model에 할당하도록 구현

--- Presentation/Feature/MatchingDetail/Sources/ValueTalk/ValueTalkViewModel.swift
+++ Presentation/Feature/MatchingDetail/Sources/ValueTalk/ValueTalkViewModel.swift
@@ -23,12 +23,17 @@ final class ValueTalkViewModel {
   
   init(getMatchValueTalkUseCase: GetMatchValueTalkUseCase) {
     self.getMatchValueTalkUseCase = getMatchValueTalkUseCase
+    Task {
+      await fetchMatchValueTalk()
+    }
   }
   
   var navigationTitle: String = Constant.navigationTitle
-  var valueTalkModel: ValueTalkModel?
   var contentOffset: CGFloat = 0
   var isNameViewVisible: Bool = true
+  private(set) var valueTalkModel: ValueTalkModel?
+  private(set) var isLoading = true
+  private(set) var error: Error?
   private let getMatchValueTalkUseCase: GetMatchValueTalkUseCase
   
   func handleAction(_ action: Action) {
@@ -39,5 +44,28 @@ final class ValueTalkViewModel {
       
     default: return
     }
+  }
+  
+  private func fetchMatchValueTalk() async {
+    do {
+      let entity = try await getMatchValueTalkUseCase.execute()
+      valueTalkModel = ValueTalkModel(
+        description: entity.shortIntroduction,
+        nickname: entity.nickname,
+        valueTalks: entity.valueTalks.map {
+          ValueTalk(
+            id: UUID(),
+            topic: $0.category,
+            summary: $0.summary,
+            answer: $0.answer
+          )
+        }
+      )
+      
+      error = nil
+    } catch {
+      self.error = error
+    }
+    isLoading = false
   }
 }
