54cd3aa25f3824066adc022d5217c61f7ef0e42c
summercat <summerly812@gmail.com>
2025-02-11 02:26:57 +0900

    [PC-544] Feat: ProfileRepository 구현

---
+++ Data/Repository/Sources/Profile/ProfileRepository.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileRepository.swift
+//  Repository
+//
+//  Created by summercat on 2/9/25.
+//
+
+import DTO
+import Entities
+import Foundation
+import PCNetwork
+import RepositoryInterfaces
+
+final class ProfileRepository: ProfileRepositoryInterface {
+  private let networkService: NetworkService
+  
+  public init (networkService: NetworkService = NetworkService()) {
+    self.networkService = networkService
+  }
+  
+  func postProfile(_ profile: ProfileModel) async throws -> PostProfileResultModel {
+    let dto = profile.toDto()
+    let endpoint = ProfileEndpoint.postProfile(dto)
+    
+    let responseDto: PostProfileResponseDTO = try await networkService.request(endpoint: endpoint)
+    return responseDto.toDomain()
+  }
+}
