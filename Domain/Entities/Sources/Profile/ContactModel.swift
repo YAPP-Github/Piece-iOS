ea3645bc21a1f3f42c3d4374acd7c71690aada96
SeoU <tjdnfkd1225@naver.com>
2025-02-12 07:08:12 +0900

    [PC-501] Feat: ContactModel id 추가

--- Domain/Entities/Sources/Profile/ContactModel.swift
+++ Domain/Entities/Sources/Profile/ContactModel.swift
@@ -5,7 +5,9 @@
 //  Created by summercat on 2/9/25.
 //
 
-public struct ContactModel {
+import Foundation
+
+public struct ContactModel: Identifiable {
   public enum ContactType: String {
     case kakao = "KAKAO_TALK_ID"
     case openKakao = "OPEN_CHAT_URL"
@@ -13,8 +15,9 @@ public struct ContactModel {
     case phone = "PHONE_NUMBER"
   }
   
+  public let id = UUID()
   public let type: ContactType
-  public let value: String
+  public var value: String
   
   public init(type: ContactType, value: String) {
     self.type = type
