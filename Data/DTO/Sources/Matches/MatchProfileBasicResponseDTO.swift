0c508a7948a9e7cc1d548b85a9456b23862b4469
summercat <summerly812@gmail.com>
2025-02-11 23:56:16 +0900

    [PC-556] Chore: 불필요한 이니셜라이저 제거

--- Data/DTO/Sources/Matches/MatchProfileBasicResponseDTO.swift
+++ Data/DTO/Sources/Matches/MatchProfileBasicResponseDTO.swift
@@ -19,30 +19,6 @@ public struct MatchProfileBasicResponseDTO: Decodable {
   public let location: String
   public let job: String
   public let smokingStatus: String
-  
-  public init(
-    matchId: Int,
-    description: String,
-    nickname: String,
-    age: Int,
-    birthYear: String,
-    height: Int,
-    weight: Int,
-    location: String,
-    job: String,
-    smokingStatus: String
-  ) {
-    self.matchId = matchId
-    self.description = description
-    self.nickname = nickname
-    self.age = age
-    self.birthYear = birthYear
-    self.height = height
-    self.weight = weight
-    self.location = location
-    self.job = job
-    self.smokingStatus = smokingStatus
-  }
 }
 
 public extension MatchProfileBasicResponseDTO {
