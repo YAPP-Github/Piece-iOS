e93cc7a9f3a3c7cca58bfa445a7955adeaf5cec4
summercat <summerly812@gmail.com>
2025-02-11 22:49:03 +0900

    [PC-504] Feat: 프로필 생성 가치관Pick 화면 구현

---
+++ Presentation/Feature/SignUp/Sources/CreateProfile/ValuePick/ValuePickView.swift
@@ -0,0 +1,120 @@
+//
+//  ValuePickView.swift
+//  SignUp
+//
+//  Created by summercat on 2/9/25.
+//
+
+import DesignSystem
+import SwiftUI
+import UseCases
+
+struct ValuePickView: View {
+  @State var viewModel: ValuePickViewModel
+  var didTapBackButton: () -> Void
+  var didTapCreateProfileButton: () -> Void
+  
+  init(
+    profileCreator: ProfileCreator,
+    getValuePicksUseCase: GetValuePicksUseCase,
+    didTapBackButton: @escaping () -> Void,
+    didTapCreateProfileButton: @escaping () -> Void
+  ) {
+    _viewModel = .init(
+      wrappedValue: .init(
+        profileCreator: profileCreator,
+        getValuePicksUseCase: getValuePicksUseCase
+      )
+    )
+    self.didTapBackButton = didTapBackButton
+    self.didTapCreateProfileButton = didTapCreateProfileButton
+  }
+  
+  var body: some View {
+    GeometryReader { proxy in
+      VStack(spacing: 0) {
+        NavigationBar(
+          title: "",
+          leftButtonTap: { didTapBackButton() }
+        )
+        
+        PCPageIndicator(step: .third, width: proxy.size.width)
+        
+        ScrollView {
+          content
+        }
+        .overlay(alignment: .bottom) {
+          if viewModel.showToast {
+            PCToast(icon: DesignSystemAsset.Icons.notice20.swiftUIImage, text: "모든 항목을 작성해 주세요")
+              .padding(.bottom, 8)
+              .onAppear {
+                withAnimation(.easeInOut(duration: 0.5)) {
+                  DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak viewModel] in
+                    viewModel?.showToast = false
+                  }
+                }
+              }
+          }
+        }
+        
+        buttonArea
+      }
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+  }
+  
+  private var content: some View {
+    VStack(alignment: .leading, spacing: 0) {
+      title
+      valuePicks
+      Spacer()
+        .frame(height: 60)
+    }
+    .frame(maxWidth: .infinity)
+  }
+  
+  private var title: some View {
+    VStack(spacing: 12) {
+      Text("가치관 Pick,\n당신의 연애 취향을 알려주세요")
+        .pretendard(.heading_L_SB)
+        .foregroundStyle(Color.grayscaleBlack)
+        .frame(maxWidth: .infinity, alignment: .leading)
+      
+      Text("선택한 답변으로 연애 스타일을 쉽게 확인할 수 있어요.\n서로의 우선순위와 취향을 이해하는 데 큰 도움이 될 거예요.")
+        .pretendard(.body_S_M)
+        .foregroundStyle(Color.grayscaleDark3)
+        .frame(maxWidth: .infinity, alignment: .leading)
+      
+    }
+    .padding(.horizontal, 20)
+    .padding(.top, 20)
+    .padding(.bottom, 16)
+  }
+  
+  private var valuePicks: some View {
+    ForEach(
+      Array(zip(viewModel.valuePicks.indices, viewModel.valuePicks)),
+      id: \.1
+    ) { index, valuePick in
+      ValuePickCard(valuePick: valuePick) { model in
+        viewModel.handleAction(.updateValuePick(model))
+      }
+      if index < viewModel.valuePicks.count - 1 {
+        Divider(weight: .thick, isVertical: false)
+      }
+    }
+  }
+  
+  private var buttonArea: some View {
+    RoundedButton(
+      type: .solid,
+      buttonText: "프로필 생성하기",
+      width: .maxWidth
+    ) {
+      viewModel.handleAction(.didTapCreateProfileButton)
+    }
+    .padding(.top, 12)
+    .padding(.horizontal, 20)
+    .background(Color.grayscaleWhite)
+  }
+}
