bfa74da7940c67d641694ba8c5492fa14dc76d84
SeoU <tjdnfkd1225@naver.com>
2025-02-12 21:19:23 +0900

    [PC-501] Feat: 프로필 기본정보 입력뷰 구현

--- Presentation/Feature/SignUp/Sources/CreateProfile/BasicInfo/CreateBasicInfoViewModel.swift
+++ Presentation/Feature/SignUp/Sources/CreateProfile/BasicInfo/CreateBasicInfoViewModel.swift
@@ -16,9 +16,24 @@ import Entities
 final class CreateBasicInfoViewModel {
   enum Action {
     case tapNextButton
+    case tapVaildNickName
     case selectCamera
     case selectPhotoLibrary
   }
+  
+  init(
+    profileCreator: ProfileCreator,
+    checkNicknameUseCase: CheckNicknameUseCase,
+    uploadProfileImageUseCase: UploadProfileImageUseCase
+  ) {
+    self.profileCreator = profileCreator
+    self.checkNicknameUseCase = checkNicknameUseCase
+    self.uploadProfileImageUseCase = uploadProfileImageUseCase
+  }
+  
+  private let checkNicknameUseCase: CheckNicknameUseCase
+  private let uploadProfileImageUseCase: UploadProfileImageUseCase
+  let profileCreator: ProfileCreator
   
   // TextField Bind
   var profileImage: UIImage? = nil
@@ -33,10 +48,27 @@ final class CreateBasicInfoViewModel {
   
   // isValid
   var isValidProfileImage: Bool = false
+  var isValidNickname: Bool = false
   var isDescriptionValid: Bool {
     !description.isEmpty && description.count <= 20
   }
+  var isValidBirthDate: Bool { isValidBirthDateFormat(birthDate) }
+  var isNextButtonEnabled: Bool {
+    return isValidProfileImage &&
+    didCheckDuplicates && isValidNickname &&
+    isValidBirthDate &&
+    !nickname.isEmpty &&
+    !description.isEmpty &&
+    !birthDate.isEmpty &&
+    !location.isEmpty &&
+    !height.isEmpty &&
+    !weight.isEmpty &&
+    !job.isEmpty &&
+    !contacts.isEmpty
+  }
+  
   var selectedItem: PhotosPickerItem? = nil
+  var didCheckDuplicates: Bool = false
   var didTapnextButton: Bool = false
   
   // TextField InfoMessage
@@ -45,11 +77,21 @@ final class CreateBasicInfoViewModel {
       return "필수 항목을 입력해 주세요."
     } else if nickname.count > 6 {
       return "6자 이하로 작성해 주세요."
+    } else if didCheckDuplicates && !isValidNickname {
+      return "이미 사용 중인 닉네임입니다."
+    } else if didCheckDuplicates && isValidNickname {
+      return "사용할 수 있는 닉네임입니다."
     } else {
       return ""
     }
   }
-  var nicknameInfoTextColor: Color = Color.systemError
+  var nicknameInfoTextColor: Color  {
+    if didCheckDuplicates && isValidNickname {
+      return Color.primaryDefault
+    } else {
+      return Color.systemError
+    }
+  }
   var descriptionInfoText: String {
     if description.isEmpty && didTapnextButton {
       return "필수 항목을 입력해 주세요."
@@ -62,6 +104,8 @@ final class CreateBasicInfoViewModel {
   var birthDateInfoText: String {
     if birthDate.isEmpty && didTapnextButton {
       return "필수 항목을 입력해 주세요."
+    } else if !birthDate.isEmpty && !isValidBirthDateFormat(birthDate) {
+      return "생년월일은 YYYYMMDD 형식으로 입력해 주세요."
     } else {
       return ""
     }
@@ -140,14 +184,52 @@ final class CreateBasicInfoViewModel {
       isCameraPresented = true
     case .selectPhotoLibrary:
       isPhotoSheetPresented = true
+    case .tapVaildNickName:
+      Task {
+        await handleTapVaildNicknameButton()
+      }
     }
   }
   
   private func handleTapNextButton() async {
-    if nickname.isEmpty || description.isEmpty || birthDate.isEmpty || location.isEmpty || height.isEmpty || weight.isEmpty || job.isEmpty || contacts.isEmpty {
+    if profileImage == nil || nickname.isEmpty || description.isEmpty || birthDate.isEmpty || location.isEmpty || height.isEmpty || weight.isEmpty || job.isEmpty || contacts.isEmpty {
       didTapnextButton = true
       await isToastVisible()
+    } else if isNextButtonEnabled {
+      do {
+        guard let imageData = profileImage?.jpegData(compressionQuality: 0.8) else {
+          print("이미지 데이터 변환 실패")
+          return
+        }
+        let imageURL = try await uploadProfileImageUseCase.execute(image: imageData)
+        
+        let basicInfo = ProfileModel(
+          nickname: nickname,
+          description: description,
+          age: calculateAge(from: birthDate),
+          birthdate: formatBirthDate(birthDate),
+          height: Int(height) ?? 0,
+          weight: Int(weight) ?? 0,
+          job: job,
+          location: location,
+          smokingStatus: smokingStatus,
+          snsActivityLevel: snsActivityLevel,
+          imageUri: imageURL.absoluteString,
+          contacts: contacts,
+          valueTalks: [],
+          valuePicks: []
+        )
+        let profile = profileCreator.createProfile()
+      } catch {
+        didTapnextButton = true
+        await isToastVisible()
+      }
     }
+  }
+  
+  private func handleTapVaildNicknameButton() async {
+    isValidNickname = (try? await checkNicknameUseCase.execute(nickname: nickname)) ?? false
+    didCheckDuplicates = true
   }
   
   @MainActor
@@ -155,6 +237,33 @@ final class CreateBasicInfoViewModel {
     showToast = true
     try? await Task.sleep(for: .seconds(3))
     showToast = false
+  }
+  
+  private func isValidBirthDateFormat(_ date: String) -> Bool {
+    let dateRegex = "^[0-9]{8}$" // YYYYMMDD
+    let dateTest = NSPredicate(format: "SELF MATCHES %@", dateRegex)
+    return dateTest.evaluate(with: date)
+  }
+  
+  private func calculateAge(from birthDateString: String) -> Int {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyyMMdd" // YYMMDD 형식
+    guard let birthDate = dateFormatter.date(from: birthDateString) else { return 0 }
+    
+    let calendar = Calendar.current
+    let now = Date()
+    
+    let ageComponents = calendar.dateComponents([.year], from: birthDate, to: now)
+    return ageComponents.year ?? 0
+  }
+  
+  private func formatBirthDate(_ birthDate: String) -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyyMMdd" // 입력된 YYYYMMDD 형식
+    guard let date = dateFormatter.date(from: birthDate) else { return "" }
+    
+    dateFormatter.dateFormat = "yyyy-MM-dd" // 서버에 전달할 형식
+    return dateFormatter.string(from: date)
   }
   
   func saveSelectedJob() {
@@ -182,8 +291,8 @@ final class CreateBasicInfoViewModel {
   
   func removeContact(at index: Int) {
     func removeContact(_ contact: ContactModel) {
-        guard contacts.count > 1 else { return } // 최소 1개는 유지
-        contacts.removeAll { $0.id == contact.id }
+      guard contacts.count > 1 else { return } // 최소 1개는 유지
+      contacts.removeAll { $0.id == contact.id }
     }
   }
   
