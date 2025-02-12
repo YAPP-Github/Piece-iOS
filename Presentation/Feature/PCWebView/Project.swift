8d762da8e316ca06ba077a41480b0f2a13888f2f
summercat <summerly812@gmail.com>
2025-02-13 00:07:57 +0900

    [PC-578] Refactor: 웹뷰를 여러 모듈에서 사용하기 위해 PCWebView 모듈로 분리

---
+++ Presentation/Feature/PCWebView/Project.swift
@@ -0,0 +1,14 @@
+//
+// Project.swift
+// PCWebView
+//
+// Created by summercat on 2025/02/12.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.dynamicFramework(
+  name: Modules.Presentation.PCWebView.rawValue,
+  dependencies: []
+)
