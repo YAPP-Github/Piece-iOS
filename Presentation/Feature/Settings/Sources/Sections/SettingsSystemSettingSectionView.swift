fde7521b61c51f7f82ae88a51f76001fbc063a1f
summercat <summerly812@gmail.com>
2025-02-13 00:33:59 +0900

    [PC-578] Feat: 설정 화면 각 섹션 뷰 구현

---
+++ Presentation/Feature/Settings/Sources/Sections/SettingsSystemSettingSectionView.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsSystemSettingSectionView.swift
+//  Settings
+//
+//  Created by summercat on 2/12/25.
+//
+
+import DesignSystem
+import SwiftUI
+
+struct SettingsSystemSettingSectionView: View {
+  let title: String
+  @Binding var isBlockingFriends: Bool
+  @Binding var date: Date
+  let didTapRefreshButton: () -> Void
+
+  var body: some View {
+    VStack(spacing: 8) {
+      SettingsSectionHeaderTitleView(title: title)
+      VStack(spacing: 0) {
+        SettingsToggleView(title: "지인 차단", isOn: $isBlockingFriends)
+        if isBlockingFriends {
+          SettingsSynchronizeContactView(
+            title: "연락처 동기화",
+            date: $date,
+            didTapRefreshButton: didTapRefreshButton
+          )
+        }
+      }
+    }
+  }
+}
