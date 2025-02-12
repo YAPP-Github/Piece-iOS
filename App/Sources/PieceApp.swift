5dfab691e1047b36ebe36538b30d1515b29e19d9
SeoU <tjdnfkd1225@naver.com>
2025-02-12 21:52:17 +0900

    [PC-498] Feat: App 구글 로그인 Kakao SDK init 추가

--- App/Sources/PieceApp.swift
+++ App/Sources/PieceApp.swift
@@ -1,13 +1,29 @@
 import DesignSystem
 import Router
+import KakaoSDKCommon
+import GoogleSignIn
 import SwiftUI
 
 @main
 struct PieceApp: App {
+ 
+  init() {
+    // Kakao SDK 초기화
+    KakaoSDK.initSDK(appKey: "NATIVE_APP_KEY")
+  }
+  
   var body: some Scene {
     WindowGroup {
       ContentView()
         .preventScreenshot() 
+        .onOpenURL { url in
+          GIDSignIn.sharedInstance.handle(url)
+        }
+        .onAppear {
+          GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+            // Check if `user` exists; otherwise, do something with `error`
+          }
+        }
     }
   }
 }
