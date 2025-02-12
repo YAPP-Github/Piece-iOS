72c707fcfe562da148b50d186db55668224b8714
summercat <summerly812@gmail.com>
2025-02-12 23:01:47 +0900

    [PC-572] Feat: 온보딩 화면 구현

---
+++ Presentation/Feature/Onboarding/Project.swift
@@ -0,0 +1,17 @@
+//
+// Project.swift
+// Onboarding
+//
+// Created by summercat on 2025/02/12.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.staticLibrary(
+  name: Modules.Presentation.Onboarding.rawValue,
+  dependencies: [
+    .presentation(target: .DesignSystem),
+    .presentation(target: .Router),
+  ]
+)
---
+++ Presentation/Feature/Onboarding/Sources/OnboardingViewModel.swift
@@ -0,0 +1,41 @@
+//
+// OnboardingViewModel.swift
+// Onboarding
+//
+// Created by summercat on 2025/02/12.
+//
+
+import DesignSystem
+import Observation
+
+@Observable
+final class OnboardingViewModel {
+  enum Action {
+    case didTapNextButton
+  }
+  
+  let onboardingContent = [
+    OnboardingContent(
+      image: DesignSystemAsset.Images.imgNoscreenshot.swiftUIImage, // TODO: - 이미지 수정
+      title: "하루 한 번,\n1:1로 만나는 특별한 인연",
+      description: "매일 밤 10시, 새로운 매칭 조각이 도착해요.\n천천히 프로필을 살펴보고, 맞춰볼지 결정해보세요."
+    ),
+    OnboardingContent(
+      image: DesignSystemAsset.Images.imgNoscreenshot.swiftUIImage,
+      title: "안심하고\n소중한 만남을 즐기세요",
+      description: "스크린샷은 제한되어 있어요.\n오직 이 공간에서만, 편안하게 인연을 찾아보세요."
+    ),
+  ]
+  var isSkipButtonVisible = true
+  var contentTabIndex: Int = 0
+  var isLastTab: Bool {
+    contentTabIndex == onboardingContent.count - 1
+  }
+  
+  func handleAction(_ action: Action) {
+    switch action {
+    case .didTapNextButton:
+      contentTabIndex += 1
+    }
+  }
+}
