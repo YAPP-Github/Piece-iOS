e13ba09c3edfa366b19d0ee1648337ac27305d5a
SeoU <tjdnfkd1225@naver.com>
2025-02-12 20:40:35 +0900

    [PC-501] Feat: 이미지 업로드 multipatfom형식 로직 추가

--- Data/PCNetwork/Sources/NetworkService.swift
+++ Data/PCNetwork/Sources/NetworkService.swift
@@ -49,4 +49,47 @@ public class NetworkService {
         }
     }
   }
+  
+  public func uploadImage<T: Decodable>(endpoint: TargetType, imageData: Data) async throws -> T {
+    return try await withCheckedThrowingContinuation { continuation in
+      session.upload(
+        multipartFormData: {
+          $0.append(
+            imageData,
+            withName: "file",
+            fileName: "image.jpg",
+            mimeType: "image/png"
+          )
+        },
+        with: endpoint)
+      .validate()
+      .responseDecodable(of: APIResponse<T>.self) { response in
+        switch response.result {
+        case .success(let apiResponse):
+          continuation.resume(returning: apiResponse.data)
+        case .failure:
+          guard let statusCode = response.response?.statusCode else {
+            continuation.resume(throwing: NetworkError.decodingFailed)
+            return
+          }
+          switch statusCode {
+          case 400:
+            let apiError: APIErrorModel? = try? JSONDecoder().decode(APIErrorModel.self, from: response.data ?? Data())
+            continuation.resume(throwing: NetworkError.badRequest(error: apiError))
+          case 401:
+            continuation.resume(throwing: NetworkError.unauthorized)
+          case 403:
+            continuation.resume(throwing: NetworkError.forbidden)
+          case 404:
+            continuation.resume(throwing: NetworkError.notFound)
+          case 500:
+            continuation.resume(throwing: NetworkError.internalServerError)
+          default:
+            continuation.resume(throwing: NetworkError.statusCode(statusCode))
+          }
+        }
+      }
+    }
+    
+  }
 }
