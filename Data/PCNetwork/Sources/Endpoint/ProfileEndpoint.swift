4ef0b2871aa6d26f8536b49bc5aac768021e0daa
SeoU <tjdnfkd1225@naver.com>
2025-02-12 21:19:51 +0900

    [PC-501] Feat: ProfileEndpoint 닉네임 중복, 이미지 업로드 추가

--- Data/PCNetwork/Sources/Endpoint/ProfileEndpoint.swift
+++ Data/PCNetwork/Sources/Endpoint/ProfileEndpoint.swift
@@ -11,28 +11,38 @@ import Foundation
 
 public enum ProfileEndpoint: TargetType {
   case postProfile(PostProfileRequestDTO)
+  case postCheckNickname(String)
+  case postUploadImage(Data)
   
   public var method: HTTPMethod {
     switch self {
     case .postProfile: .post
+    case .postCheckNickname: .post
+    case .postUploadImage: .post
     }
   }
   
   public var path: String {
     switch self {
     case .postProfile: "api/profiles"
+    case .postCheckNickname: "api/profiles/check-nickname"
+    case .postUploadImage: "api/profiles/images"
     }
   }
   
   public var headers: [String : String] {
     switch self {
     case .postProfile: [:]
+    case .postCheckNickname: [NetworkHeader.accept : NetworkHeader.all]
+    case .postUploadImage: [NetworkHeader.contentType : NetworkHeader.multipartFormData]
     }
   }
   
   public var requestType: RequestType {
     switch self {
     case let .postProfile(dto): .body(dto)
+    case let .postCheckNickname(string): .query([URLQueryItem(name: "nickname", value: string)])
+    case let .postUploadImage(data): .multipart(data)
     }
   }
 }
