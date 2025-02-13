f20b3757f8193d0de653116dee3fb65118ff5ac0
김도형 <shapekim98@gmail.com>
2025-02-13 19:10:32 +0900

    Feat: 회웥탈퇴 화면 구성

--- Presentation/Feature/Withdraw/Sources/WithdrawView.swift
+++ Presentation/Feature/Withdraw/Sources/WithdrawView.swift
@@ -10,10 +10,131 @@ import DesignSystem
 
 struct WithdrawView: View {
   @State var viewModel: WithdrawViewModel
-
+  
+  init(viewModel: WithdrawViewModel) {
+    self.viewModel = viewModel
+    UITextView.appearance().textContainerInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+  }
+  
   var body: some View {
-      VStack(spacing: 40) {
+    VStack(spacing: 0) {
+      ScrollView {
+        scrollContent
+          .padding(.horizontal, 20)
       }
+      
+      Spacer()
+      
+      RoundedButton(
+        type: viewModel.isValid ? .solid : .disabled,
+        buttonText: "다음",
+        width: .maxWidth,
+        action: { }
+      )
+      .animation(.easeInOut, value: viewModel.isValid)
+      .padding(.horizontal, 20)
+      .padding(.vertical, 12)
+    }
   }
 }
 
+private extension WithdrawView {
+  var scrollContent: some View {
+    VStack(alignment: .leading, spacing: 40) {
+      title
+      
+      radioList
+    }
+  }
+  
+  var title: some View {
+    VStack(alignment: .leading, spacing: 12) {
+      Text("지금 탈퇴하면\n새로운 인연을 만날 수 없어요")
+        .pretendard(.heading_L_SB)
+        .foregroundStyle(.grayscaleBlack)
+      
+      Text("탈퇴하는 이유를 알려주세요.\n반영하여 더 나은 사용자 경험을 제공하겠습니다.")
+        .pretendard(.body_S_M)
+        .foregroundStyle(.grayscaleDark3)
+    }
+  }
+  
+  var radioList: some View {
+    VStack(spacing: 0) {
+      ForEach(WithdrawType.allCases, id: \.self) { type in
+        radioListCell(type: type)
+      }
+      
+      if viewModel.currentWithdraw == .기타 {
+        textEditor
+      }
+    }
+  }
+  
+  func radioListCell(type: WithdrawType) -> some View {
+    HStack(spacing: 12) {
+      PCRadio(isSelected: Binding<Bool>(
+        get: { viewModel.currentWithdraw == type },
+        set: { viewModel.handleAction(.bindingWithdraw($0 ? type : nil)) }
+      ))
+      .padding(.leading, 14)
+      .animation(.easeInOut, value: viewModel.currentWithdraw)
+      
+      Text(type.rawValue)
+        .foregroundStyle(.grayscaleBlack)
+        .pretendard(.body_M_R)
+        .padding(.vertical, 14)
+      
+      Spacer(minLength: 14)
+    }
+  }
+  
+  @ViewBuilder
+  var textEditor: some View {
+    VStack(spacing: 16) {
+      TextEditor(text: Binding(
+        get: { viewModel.editorText ?? "" },
+        set: { viewModel.handleAction(.bindingEditorText($0)) }
+      ))
+      .autocorrectionDisabled()
+      .textInputAutocapitalization(.never)
+      .scrollContentBackground(.hidden)
+      .foregroundStyle(.grayscaleBlack)
+      .background(alignment: .topLeading) {
+        if viewModel.editorText?.isEmpty ?? true {
+          Text("자유롭게 작성해주세요")
+            .foregroundStyle(.grayscaleDark3)
+            .padding(.leading, 4)
+        }
+      }
+      .pretendard(.body_M_M)
+      
+      textCount
+    }
+    .padding(.horizontal, 16)
+    .padding(.vertical, 14)
+    .background(.grayscaleLight3)
+    .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+    .clipped()
+    .frame(minHeight: 160)
+  }
+  
+  var textCount: some View {
+    let count = viewModel.editorText?.count ?? 0
+    var text = AttributedString("\(count)")
+    text.setAttributes(AttributeContainer([
+      .foregroundColor: UIColor(.primaryDefault)
+    ]))
+    
+    return HStack {
+      Spacer()
+      
+      Text(text + "/100")
+        .pretendard(.body_S_M)
+        .foregroundStyle(.grayscaleDark3)
+        .contentTransition(.numericText())
+    }
+    .opacity(count == 0 ? 0 : 1)
+    .animation(.default, value: count)
+  }
+}
--- Presentation/Feature/Withdraw/Sources/WithdrawViewModel.swift
+++ Presentation/Feature/Withdraw/Sources/WithdrawViewModel.swift
@@ -9,5 +9,28 @@ import Foundation
 
 @Observable
 final class WithdrawViewModel {
-  enum Action { }
+  enum Action {
+    case bindingWithdraw(WithdrawType?)
+    case bindingEditorText(String?)
+  }
+  
+  private(set) var currentWithdraw: WithdrawType?
+  private(set) var editorText: String?
+  var isValid: Bool {
+    guard currentWithdraw != nil else { return false }
+    guard currentWithdraw != .기타 else {
+      return editorText?.count ?? 0 > 0
+    }
+    return true
+  }
+  
+  func handleAction(_ action: Action) {
+    switch action {
+    case .bindingWithdraw(let withdraw):
+      currentWithdraw = withdraw
+    case .bindingEditorText(let text):
+      guard (text?.count ?? 0) <= 100 else { return }
+      editorText = text
+    }
+  }
 }
