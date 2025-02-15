6de55aac47c19921c7975ba216b70bbfbacd71ba
summercat <summerly812@gmail.com>
2025-02-15 14:29:44 +0900

    [PC-572] Feat: Splash 모듈 생성

---
+++ Presentation/Feature/Splash/Project.swift
@@ -0,0 +1,19 @@
+//
+// Project.swift
+// Splash
+//
+// Created by summercat on 2025/02/14.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.staticLibrary(
+  name: Modules.Presentation.Splash.rawValue,
+  dependencies: [
+    .domain(target: .Entities),
+    .domain(target: .UseCases),
+    .presentation(target: .DesignSystem),
+    .presentation(target: .Router),
+  ]
+)
---
+++ Presentation/Feature/Splash/Sources/SplashView.swift
@@ -0,0 +1,17 @@
+//
+// SplashView.swift
+// Splash
+//
+// Created by summercat on 2025/02/14.
+//
+
+import SwiftUI
+import DesignSystem
+
+struct SplashView: View {
+  @State var viewModel: SplashViewModel
+
+  var body: some View {
+  
+  }
+}
