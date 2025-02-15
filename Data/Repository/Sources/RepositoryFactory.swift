c538b4bb9d2ce270fc742117d4bff643d66e879a
summercat <summerly812@gmail.com>
2025-02-16 02:04:11 +0900

    [PC-510] Feat: 사용자 신고 엔드포인트, 레포지토리, 유즈케이스 구현

--- Data/Repository/Sources/RepositoryFactory.swift
+++ Data/Repository/Sources/RepositoryFactory.swift
@@ -55,6 +55,9 @@ public struct RepositoryFactory {
     MatchesRepository(networkService: networkService)
   }
   
+  public func createReportsRepository() -> ReportsRepositoryInterface {
+    ReportsRepository(networkService: networkService)
+
   public func createSSERepository() -> SSERepositoryInterface {
     SSERepository(networkService: networkService)
   }
