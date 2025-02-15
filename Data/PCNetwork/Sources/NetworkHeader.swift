93d97c989f27c421585fb3826c676e4be9498dfe
SeoU <tjdnfkd1225@naver.com>
2025-02-15 15:52:25 +0900

    [PC-497] Feat: NetworkHeader 새로운 헤더타입 추가

--- Data/PCNetwork/Sources/NetworkHeader.swift
+++ Data/PCNetwork/Sources/NetworkHeader.swift
@@ -13,4 +13,8 @@ enum NetworkHeader {
   static let contentType = "Content-Type"
   static let applicationJson = "application/json"
   static let multipartFormData = "multipart/form-data"
+  static let authorization = "Authorization"
+  static func bearer(_ token: String) -> String {
+    return "Bearer \(token)"
+  }
 }
