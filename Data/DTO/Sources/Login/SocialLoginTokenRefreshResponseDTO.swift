1081d68b6466a63fc45925245d083a81985c28a8
summercat <summerly812@gmail.com>
2025-02-15 16:32:48 +0900

    [PC-572] Feat: 토큰 만료 시 토큰 리프레시 기능 구현

---
+++ Data/DTO/Sources/Login/SocialLoginTokenRefreshResponseDTO.swift
@@ -0,0 +1,23 @@
+//
+//  SocialLoginTokenRefreshResponseDTO.swift
+//  DTO
+//
+//  Created by summercat on 2/15/25.
+//
+
+import Entities
+import Foundation
+
+public struct SocialLoginTokenRefreshResponseDTO: Decodable {
+  public let accessToken: String
+  public let refreshToken: String
+}
+
+public extension SocialLoginTokenRefreshResponseDTO {
+  func toDomain() -> SocialLoginRefreshTokenModel {
+    SocialLoginRefreshTokenModel(
+      accessToken: accessToken,
+      refreshToken: refreshToken
+    )
+  }
+}
--- Data/PCNetwork/Sources/Endpoint/LoginEndpoint.swift
+++ Data/PCNetwork/Sources/Endpoint/LoginEndpoint.swift
@@ -11,32 +11,33 @@ import DTO
 
 public enum LoginEndpoint: TargetType {
   case loginWithOAuth(body: SocialLoginRequsetDTO)
+  case socialLoginTokenRefresh(body: SocialLoginTokenRefreshRequestDTO)
   
   public var headers: [String : String] {
     switch self {
-    case .loginWithOAuth:
-      [NetworkHeader.contentType: NetworkHeader.applicationJson]
+    case .loginWithOAuth: [NetworkHeader.contentType: NetworkHeader.applicationJson]
+    case .socialLoginTokenRefresh: [NetworkHeader.contentType: NetworkHeader.applicationJson]
     }
   }
   
   public var method: HTTPMethod {
     switch self {
-    case .loginWithOAuth:
-        .post
+    case .loginWithOAuth: .post
+    case .socialLoginTokenRefresh: .patch
     }
   }
   
   public var path: String {
     switch self {
-    case .loginWithOAuth:
-      "api/login/oauth"
+    case .loginWithOAuth: "api/login/oauth"
+    case .socialLoginTokenRefresh: "/api/login/token/refresh"
     }
   }
   
   public var requestType: RequestType {
     switch self {
-    case .loginWithOAuth(let body):
-        .body(body)
+    case .loginWithOAuth(let body): .body(body)
+    case let .socialLoginTokenRefresh(body): .body(body)
     }
   }
 }
