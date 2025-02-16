9ef2aad8c7a562e9d20fa3d7b2004db95cc6470b
summercat <summerly812@gmail.com>
2025-02-17 02:41:28 +0900

    [PC-630] Build: App BundleId 원복
    - 변경하면 인증서 다시 발급받아야해서..

--- Tuist/ProjectDescriptionHelpers/AppContants.swift
+++ Tuist/ProjectDescriptionHelpers/AppContants.swift
@@ -10,8 +10,8 @@ import ProjectDescription
 public enum AppConstants {
   public static let appName: String = "Piece-iOS"
   public static let organizationName: String = "puzzly"
-  public static let bundleId: String = "com.puzzly.piece"
   public static let version: Plist.Value = "1.0.0"
+  public static let bundleId: String = "com.piece.puzzly"
   public static let build: Plist.Value = "1"
   public static let destinations: Set<Destination> = .iOS
   public static let deploymentTargets: DeploymentTargets = .iOS("17.0")
