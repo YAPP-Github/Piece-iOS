1f87137e9e5f0d21b554d54af120f2dac20ac47c
SeoU <tjdnfkd1225@naver.com>
2025-02-22 11:23:24 +0900

    [PC-633] Feat: 탈퇴 DTO 수정

--- Data/Repository/Sources/Withdraw/WithdrawRepository.swift
+++ Data/Repository/Sources/Withdraw/WithdrawRepository.swift
@@ -17,18 +17,9 @@ private let networkService: NetworkService
     self.networkService = networkService
   }
   
-  public func deleteUserAccount(reason: String) async throws -> VoidModel {
-    let body = WithdrawRequestDTO(reason: reason)
-    let endpoint = UserEndpoint.withdrawWithPiece(body)
-    let response: VoidResponseDTO = try await networkService.request(endpoint: endpoint)
-    return response.toDomain()
-  }
-  public func withdrawWithApple() async throws -> VoidModel {
-    
-    // TODO: - 애플 전용 탈퇴 (미완)
-    /// 추후 서버와 맞춰볼 필요있음.
-    let body = WithdrawRequestDTO(reason: "")
-    let endpoint = UserEndpoint.withdrawWithPiece(body)
+  public func deleteUserAccount(providerName: String, oauthCredential: String, reason: String) async throws -> VoidModel {
+    let body = WithdrawRequestDTO(providerName: providerName, oauthCredential: oauthCredential, reason: reason)
+    let endpoint = CommonEndpoint.withdrawWithPiece(body)
     let response: VoidResponseDTO = try await networkService.request(endpoint: endpoint)
     return response.toDomain()
   }
