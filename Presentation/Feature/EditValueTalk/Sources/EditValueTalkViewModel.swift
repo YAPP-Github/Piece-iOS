34755828cafe70337ed6e5ce3590a6a55a44c7f5
summercat <summerly812@gmail.com>
2025-02-23 18:16:36 +0900

    [PC-605] Feat: 가치관Talk 편집 화면 AI 요약 기능 구현

--- Presentation/Feature/EditValueTalk/Sources/EditValueTalkViewModel.swift
+++ Presentation/Feature/EditValueTalk/Sources/EditValueTalkViewModel.swift
@@ -12,8 +12,10 @@ import UseCases
 @Observable
 final class EditValueTalkViewModel {
   enum Action {
+    case onAppear
     case updateValueTalk(ProfileValueTalkModel)
     case didTapSaveButton
+    case onDisappear
   }
   
   var valueTalks: [ProfileValueTalkModel] = []
@@ -26,13 +28,21 @@ final class EditValueTalkViewModel {
   private(set) var initialValueTalks: [ProfileValueTalkModel] = []
   private let getProfileValueTalksUseCase: GetProfileValueTalksUseCase
   private let updateProfileValueTalksUseCase: UpdateProfileValueTalksUseCase
+  private let connectSseUseCase: ConnectSseUseCase
+  private let disconnectSseUseCase: DisconnectSseUseCase
+  
+  private var sseTask: Task<Void, Never>?
   
   init(
     getProfileValueTalksUseCase: GetProfileValueTalksUseCase,
-    updateProfileValueTalksUseCase: UpdateProfileValueTalksUseCase
+    updateProfileValueTalksUseCase: UpdateProfileValueTalksUseCase,
+    connectSseUseCase: ConnectSseUseCase,
+    disconnectSseUseCase: DisconnectSseUseCase
   ) {
     self.getProfileValueTalksUseCase = getProfileValueTalksUseCase
     self.updateProfileValueTalksUseCase = updateProfileValueTalksUseCase
+    self.connectSseUseCase = connectSseUseCase
+    self.disconnectSseUseCase = disconnectSseUseCase
     
     Task {
       await fetchValueTalks()
@@ -41,26 +51,34 @@ final class EditValueTalkViewModel {
   
   func handleAction(_ action: Action) {
     switch action {
-    case let .updateValueTalk(model):
-      if let index = valueTalks.firstIndex(where: { $0.id == model.id }) {
-        valueTalks[index] = model
+    case .onAppear:
+      Task {
+        await connectSse()
       }
       
+    case let .updateValueTalk(model):
+      handleValueTalkUpdate(model)
+      
     case .didTapSaveButton:
-      didTapSaveButton()
+      Task {
+        await didTapSaveButton()
+      }
+      
+    case .onDisappear:
+      Task {
+        await disconnectSse()
+      }
     }
   }
-
-  private func didTapSaveButton() {
+  
+  private func didTapSaveButton() async {
     if isEditing {
       if isEdited {
         for cardViewModel in cardViewModels {
           valueTalks[cardViewModel.index].answer = cardViewModel.localAnswer
           valueTalks[cardViewModel.index].summary = cardViewModel.localSummary
         }
-        Task {
-          await updateProfileValueTalks()
-        }
+        await updateProfileValueTalks()
       }
     } else {
       isEditing = true
@@ -72,9 +90,28 @@ final class EditValueTalkViewModel {
       let valueTalks = try await getProfileValueTalksUseCase.execute()
       initialValueTalks = valueTalks
       self.valueTalks = valueTalks
+      cardViewModels = valueTalks.enumerated().map { index, valueTalk in
+        EditValueTalkCardViewModel(
+          model: valueTalk,
+          index: index,
+          isEditingAnswer: false,
+          onModelUpdate: { [weak self] updatedModel in
+            self?.handleValueTalkUpdate(updatedModel)
+          }
+        )
+      }
       print(valueTalks)
     } catch {
       print(error)
+    }
+  }
+  
+  private func handleValueTalkUpdate(_ model: ProfileValueTalkModel) {
+    Task { @MainActor in
+      if let index = valueTalks.firstIndex(where: { $0.id == model.id }) {
+        valueTalks[index] = model
+        cardViewModels[index].model = model
+      }
     }
   }
   
@@ -83,9 +120,40 @@ final class EditValueTalkViewModel {
       _ = try await updateProfileValueTalksUseCase.execute(valueTalks: valueTalks)
       initialValueTalks = valueTalks
       isEditing = false
-      // TODO: - AI 요약 요청 API 호출
     } catch {
       print(error)
+    }
+  }
+  
+  // MARK: - AI 요약 관련
+  
+  private func connectSse() async {
+    sseTask = Task { @MainActor in
+      do {
+        for try await createdSummary in connectSseUseCase.execute() {
+          handleSummaryUpdate(createdSummary)
+        }
+      } catch {
+        print(error)
+      }
+    }
+  }
+  
+  
+  private func disconnectSse() async {
+    do {
+      _ = try await disconnectSseUseCase.execute()
+      sseTask?.cancel()
+      
+    } catch {
+      print(error)
+    }
+  }
+  
+  private func handleSummaryUpdate(_ summary: AISummaryModel) {
+    if let index = valueTalks.firstIndex(where: { $0.id == summary.profileValueTalkId }) {
+      valueTalks[index].summary = summary.summary
+      cardViewModels[index].updateSummary(summary.summary)
     }
   }
 }
