d92e12f797b8c9444295ff1674fb39d7fc975fff
summercat <summerly812@gmail.com>
2025-02-26 02:00:27 +0900

    [PC-678] Feat: 가치관Talk TextEditor가 아닌 영역을 탭 했을 때 키보드가 내려가도록 수정

--- Presentation/Feature/SignUp/Sources/CreateProfile/ValueTalk/ValueTalkCard.swift
+++ Presentation/Feature/SignUp/Sources/CreateProfile/ValueTalk/ValueTalkCard.swift
@@ -10,11 +10,15 @@ import Entities
 import SwiftUI
 
 struct ValueTalkCard: View {
-  @FocusState var isEditing: Bool
+  private var focusState: FocusState<ValueTalkView.Field?>.Binding
   @Binding private var viewModel: ValueTalkCardViewModel
 
-  init(viewModel: Binding<ValueTalkCardViewModel>) {
+  init(
+    viewModel: Binding<ValueTalkCardViewModel>,
+    focusState: FocusState<ValueTalkView.Field?>.Binding
+  ) {
     self._viewModel = viewModel
+    self.focusState = focusState
   }
   
   var body: some View {
@@ -31,12 +35,20 @@ struct ValueTalkCard: View {
     .padding(.horizontal, 20)
     .padding(.vertical, 24)
     .background(Color.grayscaleWhite)
+    .contentShape(Rectangle())
+    .onTapGesture {
+      focusState.wrappedValue = nil
+    }
   }
   
   private var question: some View {
     VStack( alignment: .leading, spacing: 6) {
       category
       title
+    }
+    .contentShape(Rectangle())
+    .onTapGesture {
+      focusState.wrappedValue = nil
     }
   }
   
@@ -67,16 +79,16 @@ struct ValueTalkCard: View {
         .scrollDisabled(true)
         .foregroundStyle(Color.grayscaleBlack)
         .background(alignment: .topLeading) {
-          if viewModel.localAnswer.isEmpty && !isEditing {
+          if viewModel.localAnswer.isEmpty && focusState.wrappedValue != .valueTalkEditor(viewModel.model.id) {
             Text(viewModel.model.placeholder)
               .pretendard(.body_M_M)
               .foregroundStyle(Color.grayscaleDark3)
               .padding(.top, 4) // 폰트 내 lineHeight로 인해서 상단이 패딩이 더 커보이는 것 보졍
           }
         }
-        .focused($isEditing)
+        .focused(focusState, equals: .valueTalkEditor(viewModel.model.id))
       
-      if !viewModel.localAnswer.isEmpty || isEditing {
+      if !viewModel.localAnswer.isEmpty || focusState.wrappedValue == .valueTalkEditor(viewModel.model.id) {
         TextCountIndicator(count: .constant(viewModel.localAnswer.count), maxCount: 300)
       }
     }
@@ -103,6 +115,10 @@ struct ValueTalkCard: View {
         .pretendard(.body_S_R)
         .foregroundStyle(Color.grayscaleDark2)
         .animation(.bouncy(duration: 1), value: viewModel.currentGuideText)
+    }
+    .contentShape(Rectangle())
+    .onTapGesture {
+      focusState.wrappedValue = nil
     }
   }
 }
