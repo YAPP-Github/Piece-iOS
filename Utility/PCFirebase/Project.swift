1984986f64fcbf4640b3c0515dbc0bd4caf73b0c
summercat <summerly812@gmail.com>
2025-03-06 02:02:32 +0900

    [PC-705] Feat: PCNotificationService에서 서버에 FCM 토큰을 전송하는 로직 추가

--- Utility/PCFirebase/Project.swift
+++ Utility/PCFirebase/Project.swift
@@ -11,6 +11,9 @@ import ProjectDescriptionHelpers
 let project = Project.dynamicResourceFramework(
   name: Modules.Utility.PCFirebase.rawValue,
   dependencies: [
+    .data(target: .PCNetwork),
+    .data(target: .Repository),
+    .domain(target: .UseCases),
     .externalDependency(dependency: .FirebaseAnalytics),
     .externalDependency(dependency: .FirebaseCore),
     .externalDependency(dependency: .FirebaseMessaging),
