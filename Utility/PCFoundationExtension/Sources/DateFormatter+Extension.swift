df0fdec6666431dd9515c9db930962131c5268b1
SeoU <tjdnfkd1225@naver.com>
2025-03-11 19:49:56 +0900

    [PC-725] Fix: Timer 시간 계산 수정

--- Utility/PCFoundationExtension/Sources/DateFormatter+Extension.swift
+++ Utility/PCFoundationExtension/Sources/DateFormatter+Extension.swift
@@ -9,11 +9,10 @@ import Foundation
 
 public extension DateFormatter {
   static func formattedTimeString(from timeInterval: TimeInterval) -> String {
-    let formatter = DateFormatter()
-    formatter.dateFormat = "HH:mm:ss"
-    formatter.locale = Locale.current
-    formatter.timeZone = TimeZone.current
-    let date = Date(timeIntervalSince1970: timeInterval)
-    return formatter.string(from: date)
+    let hours = Int(timeInterval) / 3600
+    let minutes = Int(timeInterval) / 60 % 60
+    let seconds = Int(timeInterval) % 60
+    
+    return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
   }
 }
