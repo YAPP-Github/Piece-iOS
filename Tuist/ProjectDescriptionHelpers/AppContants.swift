a3838bf90c9ee7989f222e3f72c5f937fbc3502c
summercat <summerly812@gmail.com>
2025-03-26 16:07:02 +0900

    [PC-784] Build: 빌드 버전 업 1.0.0(6)

--- Tuist/ProjectDescriptionHelpers/AppContants.swift
+++ Tuist/ProjectDescriptionHelpers/AppContants.swift
@@ -13,7 +13,7 @@ public enum AppConstants {
   public static let bundleId: String = "com.puzzly.piece"
   public static let bundleDisplayName: Plist.Value = "피스"
   public static let version: Plist.Value = "1.0.0"
-  public static let build: Plist.Value = "2"
+  public static let build: Plist.Value = "6"
   public static let destinations: Set<Destination> = .iOS
   public static let deploymentTargets: DeploymentTargets = .iOS("17.0")
 }
