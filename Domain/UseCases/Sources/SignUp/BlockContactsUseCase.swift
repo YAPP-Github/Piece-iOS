66908dcb1164e2bbf169c0a72919540063004dd9
summercat <summerly812@gmail.com>
2025-05-03 17:47:22 +0900

    [PC-896] Chore: 연락처 차단 시 인코딩 전 전화번호 형식을 01012345678로 통일

--- Domain/UseCases/Sources/SignUp/BlockContactsUseCase.swift
+++ Domain/UseCases/Sources/SignUp/BlockContactsUseCase.swift
@@ -24,8 +24,22 @@ final class BlockContactsUseCaseImpl: BlockContactsUseCase {
   }
   
   func execute(phoneNumbers: [String]) async throws -> VoidModel {
-    let encodedContacts = phoneNumbers.compactMap { $0.data(using: .utf8)?.base64EncodedString() }
+    let encodedContacts = phoneNumbers
+      .compactMap { standardizePhoneNumber($0) }
+      .compactMap { $0.data(using: .utf8)?.base64EncodedString() }
     let blockContactsModel = BlockContactsModel(phoneNumbers: encodedContacts)
     return try await repository.postBlockContacts(phoneNumbers: blockContactsModel)
+  }
+  
+  private func standardizePhoneNumber(_ phoneNumber: String) -> String {
+      // Remove all non-numeric characters
+      let numericOnly = phoneNumber.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+      
+      // If the number starts with +82, replace it with 0
+      if numericOnly.hasPrefix("82") {
+          return "0" + numericOnly.dropFirst(2)
+      } else {
+          return numericOnly
+      }
   }
 }
